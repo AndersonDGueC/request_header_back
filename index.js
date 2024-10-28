dcc0d515c37aa488effe30c8e497e9a793f7f76e
Anderson Guerrero <adguerreroc@unal.edu.co>
2024-10-28 18:57:36 -0500

    first project microservice backend request header parser

--- index.js
+++ index.js
@@ -20,9 +20,14 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', (req, res) =>{
+//  res.json({ greeting: 'hello API' });
+//});
+res.json({"ipaddress":req.headers.host,
+          "language":req.headers['accept-language'],
+          "software":req.headers['user-agent']
+})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
